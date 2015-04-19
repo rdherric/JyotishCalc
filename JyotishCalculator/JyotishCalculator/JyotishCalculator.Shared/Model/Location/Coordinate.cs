2aadbc51c1e524ee8ba9c7229f4cafdb87d55bd8
Robert D. Herrick II <robert.herrick@rdh2.com>
2015-04-19 11:12:47 -0600

    Created Universal App and began on model with Profiles

---
+++ JyotishCalculator/JyotishCalculator/JyotishCalculator.Shared/Model/Location/Coordinate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JyotishCalculator.Model.Location
+{
+    /// <summary>
+    /// Coordinate represents a Latitude or Longitude coordinate
+    /// on the Earth in Degrees, Minutes, and Seconds.
+    /// </summary>
+    public struct Coordinate
+    {
+        /// <summary>
+        /// Degrees gets and sets the Degrees of the Coordinate.
+        /// </summary>
+        public int Degrees { get; set; }
+
+
+        /// <summary>
+        /// Minutes gets and sets the Minutes of the Coordinate.
+        /// </summary>
+        public int Minutes { get; set; }
+
+
+        /// <summary>
+        /// Seconds gets and sets the Seconds of the Coordinate.
+        /// </summary>
+        public int Seconds { get; set; }
+    }
+}
