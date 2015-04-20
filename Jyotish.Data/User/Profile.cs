a265053f2f5a7f8dc91c454de99c1e90959a2e5e
Robert D. Herrick II <robert.herrick@rdh2.com>
2015-04-20 10:45:22 -0600

    Finished rename of all parts of the application

---
+++ Jyotish.Data/User/Profile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using JyotishCalculator.Model.Location;
+
+namespace JyotishCalculator.Model.User
+{
+    /// <summary>
+    /// Profile stores a Jyotish profile for a User.
+    /// </summary>
+    public class Profile
+    {
+        /// <summary>
+        /// ID gets and sets the ID of the Profile for creating and
+        /// deleting from the Profiles collection.
+        /// </summary>
+        public int ID { get; set; }
+
+        
+        /// <summary>
+        /// Name gets and sets the Name of the User that represents 
+        /// the Profile.
+        /// </summary>
+        public string Name { get; set; }
+
+
+        /// <summary>
+        /// BirthDateTime gets and sets the DateTime of the User for 
+        /// this Profile.
+        /// </summary>
+        public DateTime BirthDateTime { get; set; }
+
+
+        /// <summary>
+        /// BirthLocation gets and sets the LatLong of the birth
+        /// location of the User for this Profile.
+        /// </summary>
+        public LatLong BirthLocation { get; set; }
+    }
+}
